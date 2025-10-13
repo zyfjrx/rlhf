ed4e3ea7d5a8711069c93c9ff64c0c0c9e378272
zhangyf <zhangyifan0129@gmail.com>
2025-10-13 09:15:54 +0800

    feat(chapter11): 添加倒计时任务测试脚本和数据集支持
    - 新增 CountdownTasksDataset 数据集类用于处理倒计时任务- 实现 reward_function用于评估模型输出的答案
    - 添加 rollout 函数用于采集训练轨迹
    - 编写 test.py 脚本测试模型在倒计时任务上的表现
    - 支持从预训练模型加载并进行推理验证- 增加对 tokenizer 和数据路径的配置支持

---
+++ chapter11/grpo_zero/test.py
@@ -0,0 +1,78 @@
+from qwen2_model import Transformer
+import torch
+from pathlib import Path
+from tokenizer import Tokenizer
+from countdown_task import CountdownTasksDataset, reward_function
+from grpo import rollout
+from torch.utils.data import DataLoader
+from pprint import pprint
+t = Tokenizer("/Users/zhangyf/llm/Qwen2.5-0.5B/tokenizer.json")
+c = CountdownTasksDataset(tokenizer=t, data_path="Countdown-Tasks-3to4")
+pprint(c.encode_prefix(numbers=[1, 2, 3], target=6)["prefix"])
+print(c.encode_prefix(numbers=[1, 2, 3], target=6)["prefix_tokens"])
+print(c.encode_prefix(numbers=[1, 2, 3], target=6)["prefix_token_ids"])
+
+response = """
+<think>我认为应该。。。</think>
+<answer>1+2+3</answer>
+"""
+
+pprint(reward_function(response=response, numbers=[1, 2, 3], target=6))
+
+
+pretrained_model_path = Path("/Users/zhangyf/llm/Qwen2.5-0.5B")
+device = torch.device("mps")
+dtype = torch.bfloat16
+
+torch.set_default_device(device)
+torch.random.manual_seed(1337)
+BATCH_SIZE = 4 # 一批数据4条
+NUM_QUESTIONS_PER_BATCH = 2 # 每批数据2个问题
+NUM_ANSWERS_PER_QUESTION = BATCH_SIZE // NUM_QUESTIONS_PER_BATCH
+
+
+train_dataset = CountdownTasksDataset(
+    data_path="Countdown-Tasks-3to4",
+    tokenizer=t,
+    split="train",
+    test_size=128,
+)
+generator = torch.Generator(device=device)
+train_dataloader = DataLoader(
+    train_dataset,
+    shuffle=True,
+    collate_fn=CountdownTasksDataset.collate_fn,
+    generator=generator,
+    batch_size=NUM_QUESTIONS_PER_BATCH,
+)
+
+print(train_dataloader)
+b = next(iter(train_dataloader))
+print(b.prefix[0])
+print(len(b.prefix))
+
+model = Transformer.from_pretrained(
+    "/Users/zhangyf/llm/Qwen2.5-0.5B", device=device).train()
+
+episodes = rollout(
+    model=model,
+    tokenizer=t,
+    batch=b,
+    max_gen_len=1024,
+    num_answer_per_question=2,
+    reward_function=reward_function,
+    device=device,
+    dtype=dtype,
+)
+print("\n=========采集轨迹===========")
+for episode in episodes:
+    print("=============prefix start=============")
+    pprint(episode.prefix)
+    print("=============prefix end===============")
+    print("===============text=======================")
+    pprint(episode.text)
+    print("===============text=======================")
+    print("===============text=======================")
+    print("===============text=======================")
+    print("===============text=======================")
+    print("===============text=======================")
